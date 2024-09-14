0d2abc7606f634e3f9bc6815e22c7ae7a4175cfc
kawsarhossen1 <kawsarhossen66@gmail.com>
2024-09-14 12:53:45 +0600

    added

--- scripts/rectangle-area.js
+++ scripts/rectangle-area.js
@@ -1,3 +1,25 @@
-function calculateRectangleArea(){
-    console.log('calculate rect area');
+function calculateRectangleArea() {
+    const rectangleLengthInput = document.getElementById('rectangle-length');
+    const rectangleText = rectangleLengthInput.value;
+    const length = parseFloat(rectangleText);
+    console.log(length);
+
+    //get width of the rectangle
+
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
+     
+
+    // calculate recangle area
+
+    const area = length * width;
+    console.log('area of the rectangle: ', area );
+
+
+    //display the rectangle area
+
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }

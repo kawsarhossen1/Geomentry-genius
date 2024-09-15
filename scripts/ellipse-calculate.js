ee328262a97aa522ae7bcd5c47f1a79958335000
kawsarhossen1 <kawsarhossen66@gmail.com>
2024-09-15 13:44:45 +0600

    added

--- scripts/ellipse-calculate.js
+++ scripts/ellipse-calculate.js
@@ -1,20 +1,10 @@
 function calculateEllipseArea() {
     const ellipsoneA = getInputValueById('ellipsone-a');
-    const ellipsoneB= getInputValueById('ellipse-b');
-    
+    const ellipsoneB = getInputValueById('ellipse-b');
 
-    const area = 3.14 * ellipsoneA  * ellipsoneB;
+
+    const area = 3.14 * ellipsoneA * ellipsoneB;
     setInnerTextById('ellipse-area', area);
 }
 
-function getInputValueById(inputFieldId) {
-    const inputField = document.getElementById(inputFieldId);
-    const inputValueText = inputField.value;
-    const value = parseFloat(inputValueText);
-    return value;
-}
 
-function setInnerTextById(elementId, area) {
-    const element = document.getElementById(elementId);
-    element.innerText = area;
-}
